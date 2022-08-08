d2380284b4d213c40389a66ace5de831d75ee2f4
viralvaghela <viralvaghela7109@gmail.com>
2022-08-08 13:18:20 +0530

    token add

--- utility/util.js
+++ utility/util.js
@@ -1,6 +1,6 @@
 const util = module.exports = {};
 
-util.token = "";//token
+util.token = "5421338315:AAFyCU_zZWbHKXKuHT5TrdVaELhRzdUZnCg";
 util.pixeles_base_url ="https://api.pexels.com/v1/search?query=";
-util.pixels_api_key=""; // pexels api key
+util.pixels_api_key="563492ad6f917000010000019e8c9190f2314cdabae714932498b9c7";
 
